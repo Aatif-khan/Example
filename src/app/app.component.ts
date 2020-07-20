070fd260a480958aec5c3861dd13b62091380989
Aatif-khan <pathanaatif6@gmail.com>
2020-07-20 17:58:48 +0530

    Back button implimantation

---
+++ src/app/app.component.ts
@@ -0,0 +1,101 @@
+import { Component, ViewChildren, QueryList, OnDestroy } from '@angular/core';
+
+import { Platform, IonRouterOutlet, AlertController } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { async } from '@angular/core/testing';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent implements OnDestroy {
+
+  backButtonSubscription;
+  @ViewChildren(IonRouterOutlet) routerOutlet: QueryList<IonRouterOutlet>;
+
+  navigate: any;
+  constructor(
+    private platform: Platform,
+    public router: Router,
+    public alertController: AlertController,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.sideMenu();
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.backButtonEvent();
+      this.splashScreen.hide();
+    });
+  }
+
+  sideMenu(){
+    this.navigate =
+    [
+      {
+        title : 'Home',
+        url   : '/home',
+        icon  : 'home'
+      },
+      {
+        title : 'Chat',
+        url   : '/chat',
+        icon  : 'chatbubbles'
+      },
+      {
+        title : 'Contacts',
+        url   : '/contacts',
+        icon  : 'people'
+      },
+    ]
+  }
+
+  backButtonEvent(){
+    this.backButtonSubscription = this.platform.backButton.subscribe(async () => {
+      this.routerOutlet.forEach((outlet: IonRouterOutlet) => {
+        if (outlet && outlet.canGoBack()){
+          outlet.pop();
+        } else if (this.router.url === '/home'){
+          this.presentAlertConfirm();
+        }
+      });
+    });
+  }
+
+    async presentAlertConfirm() {
+      const alert = await this.alertController.create({
+        header: 'Confirm!',
+        message: 'Confirm to exit App !!!',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: (blah) => {
+              console.log('Confirm Cancel: blah');
+            }
+          }, {
+            text: 'Exit',
+            handler: () => {
+              console.log('Confirm Okay');
+              navigator['app'].exitApp();
+            }
+          }
+        ]
+      });
+      await alert.present();
+    }
+
+    ngOnDestroy() {
+      this.backButtonSubscription.unsbscribe();
+    }
+
+
+}
